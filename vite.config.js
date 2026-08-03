8c04ab8b07978ba697abf0596693dcfc979e7800
srinismile <105926422+srinismile@users.noreply.github.com>
2026-08-03 21:34:19 +0530

    Fix entry script path in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nlcil-ppt-generator/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets', // Forces Vite to place JS and CSS in dist/assets
-  },
+  base: '/nlcil-ppt-generator/', // Exact repo name with slashes on both ends
 })
