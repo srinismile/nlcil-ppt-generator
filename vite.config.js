1d174299c2718477bbdfeb47377826bb99794e3f
srinismile <105926422+srinismile@users.noreply.github.com>
2026-08-03 21:24:34 +0530

    Fix build output and node version

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/nlcil-ppt-generator/',
   build: {
     outDir: 'dist',
-  }
+    assetsDir: 'assets', // Forces Vite to place JS and CSS in dist/assets
+  },
 })
